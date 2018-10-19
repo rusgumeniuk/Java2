9a7e90a1eff6f467c457b8455de4d3ca662d1780
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-19 23:13:22 +0300

    some using

--- Lab1SimpleData/src/simpleData/SimpleData.java
+++ Lab1SimpleData/src/simpleData/SimpleData.java
@@ -1,5 +1,6 @@
 package simpleData;
 import simpleData.dao.StudentJdbcDao;
+import simpleData.dao.StudentNoSqlDao;
 
 import java.sql.*;
 import java.io.*;
@@ -8,8 +9,8 @@ import java.nio.file.*;
 
 public class SimpleData {
     public static void main(String[] args) throws IOException, SQLException {
-        StudentJdbcDao dao = new StudentJdbcDao();
-        Connection obj = StudentJdbcDao.getConnection();
-        System.out.println(obj.isClosed());
+        StudentNoSqlDao dao = new StudentNoSqlDao();
+        List<Student> result =  dao.getAll();
+        System.out.println(result);
     }
 }
